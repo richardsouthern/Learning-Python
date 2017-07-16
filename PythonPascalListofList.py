1d9b41f321da5a2f831601eaf282df0b6a3cccf5
richardsouthern <richard_southern@hotmail.com>
2017-07-16 14:30:21 -0600

    Add files via upload

--- PythonPascalListofList.py
+++ PythonPascalListofList.py
@@ -18,4 +18,33 @@ with open('PascalListofList','w') as f:
     s = str(matrix)
     f.write(s)
     f.close()
-
+#higher order binomisl ot Pascal numbers
+# column two is the lazy cater's number A000124 
+# column three is the cake numbers A000125
+# column A000126 
+H = [[], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], []]   
+n = 0
+while n < 19:
+        sum = 0
+        k = 0
+        while k < 19:
+            if k <= n:
+                sum = sum + matrix [n] [k]
+                # new column in row n
+                H [n] .append(sum)
+            #    print(n, k,sum)
+            else:
+                H [n] .append(sum)
+            # next column
+            k = k + 1
+        # next row
+        sum = 0
+        n = n + 1
+# display list higher order Pascal
+for row in H:
+    print(row)
+# save as string 
+with open('HighLPascal','w') as f:
+    s = str(H)
+    f.write(s)
+    f.close()     
